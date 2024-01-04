18b5487cd41967a83627a68ee5ab646215e19868
Cole <cperkins577@gmail.com>
2024-01-03 21:18:17 -0500

    Finished character node prototype and organized files

---
+++ scripts/player.gd
@@ -0,0 +1,29 @@
+extends Area2D
+@export var speed = 400
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# Scripts to set movement vectors when key is pressed
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	
+	# Normalizes the vectors to maintain constant speed in all directions
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
