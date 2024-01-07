d61cc860a5bdd74ad3aa74cf613e3ab51b66c24c
Cole <cperkins577@gmail.com>
2024-01-07 01:45:12 -0500

    Added animations to movement and idle

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
-@export var speed = 400
+@export var speed = 200
+@onready var animation_tree : AnimationTree = $AnimationTree
 var screen_size
 var current_dir
 
@@ -7,6 +8,7 @@ var current_dir
 func _ready():
 	screen_size = get_viewport_rect().size
 	current_dir = "down"
+	animation_tree.active = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,53 +17,34 @@ func _process(delta):
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
-		current_dir = "up"
-		play_anim("move",current_dir)
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
-		current_dir = "down"
-		play_anim("move",current_dir)
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
-		current_dir = "left"
-		play_anim("move",current_dir)
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
-		current_dir = "right"
-		play_anim("move",current_dir)
 	if velocity.x==0 and velocity.y==0:
-		play_anim("idle",current_dir)
-	
+		pass
 	# Normalizes the vectors to maintain constant speed in all directions
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-	
+	update_anim_para()
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+	move_and_slide()
+
+func update_anim_para():
+	if velocity == Vector2.ZERO:
+		animation_tree["parameters/conditions/is_idle"] = true
+		animation_tree["parameters/conditions/is_walking"] = false
+	else:
+		animation_tree.set("parameters/idle/blend_position", velocity)
+		animation_tree.set("parameters/walk/blend_position", velocity)
+		animation_tree.set("parameters/attack/blend_position", velocity)
+		animation_tree["parameters/conditions/is_idle"] = false
+		animation_tree["parameters/conditions/is_walking"] = true
 	
-func play_anim(type, dir):
-	var anim = $AnimatedSprite2D
-	if type == "move":
-		match dir:
-			"up":
-				anim.play("up_walk")
-			"down":
-				anim.play("down_walk")
-			"left":
-				anim.play("left_walk")
-			"right":
-				anim.play("right_walk")
-			_:
-				anim.play("down_walk")
-	if type == "idle":
-		match dir:
-			"up":
-				anim.play("up_idle")
-			"down":
-				anim.play("down_idle")
-			"left":
-				anim.play("left_idle")
-			"right":
-				anim.play("right_idle")
-			_:
-				anim.play("down_idle")
+	if Input.is_action_just_pressed("attack"):
+		animation_tree["parameters/conditions/attack"] = true
+	else:
+		animation_tree["parameters/conditions/attack"] = false
