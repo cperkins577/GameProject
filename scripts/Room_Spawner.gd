04c89e840032bb7ad977f1dea3682a1d98ab9675
Cole <cperkins577@gmail.com>
2024-01-11 22:42:24 -0500

    debugging

--- scripts/Room_Spawner.gd
+++ scripts/Room_Spawner.gd
@@ -21,39 +21,40 @@ func is_spawned():
 	return spawned
 
 func spawn():
-		match opening_direction:
-			1:
-				var bottom_rooms = templates.get("bottom_rooms")
-				var rng = randi_range(0,bottom_rooms.size()-1)
-				var scene = bottom_rooms[rng]
-				var instance = scene.instantiate()
-				instance.position = position
-				get_parent().add_sibling.call_deferred(instance)
-			2:
-				var right_rooms = templates.get("right_rooms")
-				var rng = randi_range(0,right_rooms.size()-1)
-				var scene = right_rooms[rng]
-				var instance = scene.instantiate()
-				instance.position = position
-				get_parent().add_sibling.call_deferred(instance)
-			3:
-				var top_rooms = templates.get("top_rooms")
-				var rng = randi_range(0,top_rooms.size()-1)
-				var scene = top_rooms[rng]
-				var instance = scene.instantiate()
-				instance.position = position
-				get_parent().add_sibling.call_deferred(instance)
-			4:
-				var left_rooms = templates.get("left_rooms")
-				var rng = randi_range(0,left_rooms.size()-1)
-				var scene = left_rooms[rng]
-				var instance = scene.instantiate()
-				instance.position = position
-				get_parent().add_sibling.call_deferred(instance)
-			_:
-				pass
-		spawned = true
-		spawned_total += 1
+	print(opening_direction)
+	match opening_direction:
+		1:
+			var bottom_rooms = templates.get("bottom_rooms")
+			var rng = randi_range(0,bottom_rooms.size()-1)
+			var scene = bottom_rooms[rng]
+			var instance = scene.instantiate()
+			instance.transform.origin = transform.origin
+			get_parent().add_sibling.call_deferred(instance)
+		2:
+			var right_rooms = templates.get("right_rooms")
+			var rng = randi_range(0,right_rooms.size()-1)
+			var scene = right_rooms[rng]
+			var instance = scene.instantiate()
+			instance.transform.origin = transform.origin
+			get_parent().add_sibling.call_deferred(instance)
+		3:
+			var top_rooms = templates.get("top_rooms")
+			var rng = randi_range(0,top_rooms.size()-1)
+			var scene = top_rooms[rng]
+			var instance = scene.instantiate()
+			instance.transform.origin = transform.origin
+			get_parent().add_sibling.call_deferred(instance)
+		4:
+			var left_rooms = templates.get("left_rooms")
+			var rng = randi_range(0,left_rooms.size()-1)
+			var scene = left_rooms[rng]
+			var instance = scene.instantiate()
+			instance.transform.origin = transform.origin
+			get_parent().add_sibling.call_deferred(instance)
+		_:
+			pass
+	spawned = true
+	spawned_total += 1
 
 
 func _on_area_2d_area_entered(area):
