83b5c2805a673b6feeb058794e7d88dcedd8ceda
Cole <cperkins577@gmail.com>
2024-01-13 13:11:41 -0500

    Various debug changes

--- scripts/Room_Spawner.gd
+++ scripts/Room_Spawner.gd
@@ -19,45 +19,60 @@ func _ready():
 
 func is_spawned():
 	return spawned
+	
+func check_overlap():
+	#print("checking")
+	await get_tree().process_frame
+	var areas = find_child("Area2D").get_overlapping_areas()
+	#print(areas)
+	for area in areas:
+		#print("overlapping")
+		if area.get_parent() is Room_Spawner:
+			#print("deleting")
+			area.get_parent().queue_free()
+
+func _on_area_2d_area_entered(area):
+	#print("deleting")
+	if area.get_parent() is Room_Spawner and area.get_parent().spawned == true:
+		#print("deleted")
+		area.get_parent().queue_free()
 
 func spawn():
-	print(opening_direction)
+	await get_tree().process_frame
+	await check_overlap()
 	match opening_direction:
 		1:
+			#print("Spawning up")
 			var bottom_rooms = templates.get("bottom_rooms")
 			var rng = randi_range(0,bottom_rooms.size()-1)
 			var scene = bottom_rooms[rng]
 			var instance = scene.instantiate()
-			instance.transform.origin = transform.origin
 			get_parent().add_sibling.call_deferred(instance)
+			instance.position = global_position
 		2:
-			var right_rooms = templates.get("right_rooms")
-			var rng = randi_range(0,right_rooms.size()-1)
-			var scene = right_rooms[rng]
-			var instance = scene.instantiate()
-			instance.transform.origin = transform.origin
-			get_parent().add_sibling.call_deferred(instance)
-		3:
-			var top_rooms = templates.get("top_rooms")
-			var rng = randi_range(0,top_rooms.size()-1)
-			var scene = top_rooms[rng]
-			var instance = scene.instantiate()
-			instance.transform.origin = transform.origin
-			get_parent().add_sibling.call_deferred(instance)
-		4:
+			#print("Spawning right")
 			var left_rooms = templates.get("left_rooms")
 			var rng = randi_range(0,left_rooms.size()-1)
 			var scene = left_rooms[rng]
 			var instance = scene.instantiate()
-			instance.transform.origin = transform.origin
 			get_parent().add_sibling.call_deferred(instance)
+			instance.position = global_position
+		3:
+			#print("Spawning down")
+			var top_rooms = templates.get("top_rooms")
+			var rng = randi_range(0,top_rooms.size()-1)
+			var scene = top_rooms[rng]
+			var instance = scene.instantiate()
+			get_parent().add_sibling.call_deferred(instance)
+			instance.position = global_position
+		#4:
+			#var right_rooms = templates.get("right_rooms")
+			#var rng = randi_range(0,right_rooms.size()-1)
+			#var scene = right_rooms[rng]
+			#var instance = scene.instantiate()
+			#get_parent().add_sibling.call_deferred(instance)
+			#instance.position = global_position
 		_:
 			pass
 	spawned = true
-	spawned_total += 1
-
-
-func _on_area_2d_area_entered(area):
-	print("deleting")
-	if area is Room_Spawner:
-		queue_free()
+	await get_tree().process_frame
